543b8ef0ea70674dca152fedd3667a81ad4866d1
toffettl <toffetti0824@gmail.com>
2025-11-15 23:23:22 -0300

    ChatGPT Function Calling

--- internal/gateway/summarize/gateway.go
+++ internal/gateway/summarize/gateway.go
@@ -4,4 +4,5 @@ import "context"
 
 type Summarize interface {
 	Resume(ctx context.Context, transcription string) (*ResumeOutput, error)
+	FullTextOrganize(ctx context.Context, transcription string) (*string, error)
 }
--- internal/infrastructure/clients/chatgpt/client.go
+++ internal/infrastructure/clients/chatgpt/client.go
@@ -1,20 +1,228 @@
 package chatgpt
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/toffettl/explicAI/internal/gateway/summarize"
+	"github.com/toffettl/explicAI/internal/infrastructure/clients"
+)
+
 const (
-	basePath = "v1/chat/completions"
-	systemPrompt = "Você é um sistema que recebe um texto transcrito de um áudio e organiza, separando em parágrafos e corrigindo possíveis erros de concordância."
-	resumeUserPrompt = "Preciso de um objeto com título sugerido, descrição sugerida, resumo breve e médio sobre a seguinte transcrição:"
-	functionCallName = "resume"
+	basePath                  = "v1/chat/completions"
+	systemPrompt              = "Você é um sistema que recebe um texto transcrito de um áudio e organiza, separando em parágrafos e corrigindo possíveis erros de concordância."
+	resumeUserPrompt          = "Preciso de um objeto com título sugerido, descrição sugerida, resumo breve e médio sobre a seguinte transcrição:"
+	fullTextOrganizeUserPromt = "Retorne apenas o texto normalizado para a seguinte transcrição:"
+	functionCallName          = "resume"
 )
 
 type (
 	ChatgptFunctionCallRequest struct {
-		Model string `json:"model"`
+		Model        string       `json:"model"`
+		Messages     []Message    `json:"messages"`
+		Functions    []Function   `json:"functions,omitempty"`
+		FunctionCall FunctionCall `json:"function_call",omitempty`
+	}
+
+	ChatgptSimpleRequest struct {
+		Model    string    `json:"model"`
 		Messages []Message `json:"messages"`
 	}
 
+	FunctionCall struct {
+		Name string `json:"name"`
+	}
+
 	Message struct {
-		Role string `json:"role"`
+		Role    string `json:"role"`
 		Content string `json:"content"`
 	}
+
+	Function struct {
+		Name       string         `json:"name"`
+		Parameters FunctionParams `json:"parameters"`
+	}
+
+	FunctionParams struct {
+		Type       string         `json:"type"`
+		Properties FunctionFields `json:"properties"`
+	}
+
+	FunctionFields struct {
+		Title        FieldSpec `json"title"`
+		Description  FieldSpec `json:"description"`
+		BriefResume  FieldSpec `json:"briefResume"`
+		MediumResume FieldSpec `json:"mediumResume"`
+	}
+
+	FieldSpec struct {
+		Type        string `json:"type"`
+		description string `json:"description"`
+	}
+
+	ChatResumeCompletionResponse struct {
+		Choices []struct {
+			Message struct {
+				FunctionCall struct {
+					Arguments string `json:"arguments"`
+				} `json:"function_call"`
+			} `json:"message"`
+		} `json:"choices"`
+	}
+
+	ChatFullTextCompletionResponse struct {
+		Choices []struct {
+			Message struct {
+				Content string `json:"content"`
+			} `json:"message"`
+		} `json:"choices"`
+	}
 )
+
+type Client struct {
+	HttpClient  *clients.BaseHttp
+	ApiKey      string
+	ServiceName string
+	Model       string
+}
+
+func NewClient(serviceName, URL, apiKey, model string, timeout int64) *Client {
+	return &Client{
+		ServiceName: serviceName,
+		HttpClient:  clients.NewHttpClient(URL, timeout),
+		ApiKey:      apiKey,
+		Model:       model,
+	}
+}
+
+func (c *Client) Resume(ctx context.Context, transcription string) (
+	*summarize.ResumeOutput, error,
+) {
+	req := c.HttpClient.Client.
+		SetHeader("Authorization", "Bearer"+c.ApiKey).
+		SetHeader("Content-Type", "application/json").
+		SetBody(c.buildResumeRequest(transcription))
+
+	res, err := req.Post(basePath)
+
+	if res.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("error on chatgpt resume request: response=%s | status=%s", res.Body(), res.Status())
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error on chatgpt resume request: error=%s", err.Error())
+	}
+
+	var chatResponse ChatResumeCompletionResponse
+	if err = json.Unmarshal(res.Body(), &chatResponse); err != nil {
+		return nil, fmt.Errorf("error on chatgpt resume request: error=%s", err.Error())
+	}
+
+	choices := chatResponse.Choices
+	if len(choices) <= 0 {
+		return nil, fmt.Errorf("error on chatgpt resume request: no choices in response")
+	}
+
+	var response summarize.ResumeOutput
+	if err = json.Unmarshal([]byte(choices[0].Message.FunctionCall.Arguments), &response); err != nil {
+		return nil, fmt.Errorf("error on chatgpt resume request: error=%s", err.Error())
+	}
+
+	return &response, nil
+}
+
+func (c *Client) FullTextOrganize(ctx context.Context, transcription string) (*string, error) {
+	req := c.HttpClient.Client.
+		SetHeader("Authorization", "Bearer"+c.ApiKey).
+		SetHeader("Content-Type", "application/json").
+		SetBody(c.buildFullTextOrganizeRequest(transcription))
+
+	res, err := req.Post(basePath)
+
+	if res.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("error on chatgpt full text organize request: response=%s | status=%s", res.Body(), res.Status())
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error on chatgpt full text organize request: error=%s", err.Error())
+	}
+
+	var response ChatFullTextCompletionResponse
+	if err = json.Unmarshal(res.Body(), &response); err != nil {
+		return nil, fmt.Errorf("error on chatgpt full text organize request: error=%s", err.Error())
+	}
+
+	if len(response.Choices) <= 0 || response.Choices[0].Message.Content == "" {
+		
+	}
+}
+
+func (c *Client) buildResumeRequest(transcription string) ChatgptFunctionCallRequest {
+	fullResumeUserPrompt := resumeUserPrompt + "/n" + transcription
+
+	return ChatgptFunctionCallRequest{
+		Model: c.Model,
+		Messages: []Message{
+			{
+				Role:    "system",
+				Content: systemPrompt,
+			},
+			{
+				Role:    "user",
+				Content: fullResumeUserPrompt,
+			},
+		},
+		Functions: buildFunctionCallRequest(),
+		FunctionCall: FunctionCall{
+			Name: functionCallName,
+		},
+	}
+}
+
+func buildFunctionCallRequest() []Function {
+	return []Function{
+		{
+			Name: functionCallName,
+			Parameters: FunctionParams{
+				Type: "object",
+				Properties: FunctionFields{
+					Title: FieldSpec{
+						Type:        "string",
+						description: "Titulo de até 60 caracteres",
+					},
+					Description: FieldSpec{
+						Type:        "string",
+						description: "Descrição de até 300 caracteres",
+					},
+					BriefResume: FieldSpec{
+						Type:        "string",
+						description: "Resume breve de até 5 linhas",
+					},
+					MediumResume: FieldSpec{
+						Type:        "string",
+						description: "Resume médio de até 15 linhas",
+					},
+				},
+			},
+		},
+	}
+}
+
+func (c *Client) buildFullTextOrganizeRequest(transcription string) ChatgptSimpleRequest {
+	fullTextOrganizePrompt := fullTextOrganizeUserPromt + "\n" + transcription
+	return ChatgptSimpleRequest{
+		Model: c.Model,
+		Messages: []Message{
+			{
+				Role:    "string",
+				Content: systemPrompt,
+			},
+			{
+				Role:    "user",
+				Content: fullTextOrganizePrompt,
+			},
+		},
+	}
+}
